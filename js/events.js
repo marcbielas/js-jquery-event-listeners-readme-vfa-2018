f6fa5ee068194e6f0d835bdcb789f385647c4f8a
Marc Bielas <marc@bielas.com>
2018-06-08 20:00:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -31,7 +31,7 @@ pressIt = function(){
 
 submitIt = function(){
   $("form").on("submit", function() {
-    alert('your form is going to be submitted now');
+    alert('Your form is going to be submitted now.');
     return;
 });
 }
