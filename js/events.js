6234b79fa70e9667dcf9d27a78d430d2714581de
Marc Bielas <marc@bielas.com>
2018-06-08 20:00:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,36 @@
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
+getIt = function(){
+  $('p').on('click', function(){
+    alert("Hey!");
+});
+}
+
+frameIt = function(){
+  $('img').on('load', function(){
+  $('img').addClass("tasty");
+});
+}
+
+pressIt = function(){
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+}
+
+submitIt = function(){
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+});
+}
